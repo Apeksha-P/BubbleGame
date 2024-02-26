1763420556bd4c508ba3e33532978e6fb181279c
Apeksha-P <pavithriapeksha1new@gmail.com>
2024-02-26 22:41:52 +0530

    content update

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded",function (){
             count = count + 5;
             div.remove();
             var display = document.getElementById('mark');
+            var over = document.getElementById('end');
+            //over.css('display','none');
             if(display){
                 display.textContent = "Score : " + count;
             }
